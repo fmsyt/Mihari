b9e91b72dd31bc53de2cf2ed9b33a5ec25909847
fmsyt <fms20yt@gmail.com>
2023-05-15 14:00:23 +0900

    initial size

--- main.py
+++ main.py
@@ -18,12 +18,23 @@ async def main(page: ft.Page):
     page.window_left = 400
     page.window_top = 200
 
+    controls = [
+        PlotCpu(),
+        PlotMemory(),
+    ]
+
+    controls_count = len(controls)
+
+    padding = 8
+    spacing = 4
+
+    page.window_width = 48 + 40 + 96 + padding * controls_count * 4 + spacing * controls_count
+    page.window_height = 64 * 2 + padding * controls_count * 2 + spacing * (controls_count - 1)
+
+
     area = ft.WindowDragArea(
         ft.ListView(
-            controls=[
-                PlotCpu(),
-                PlotMemory(),
-            ],
+            controls,
             spacing=4,
         ),
         width=page.width,
