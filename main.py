310762a66036618ae7a2153b331dde542fe16194
fmsyt <fms20yt@gmail.com>
2023-05-16 00:04:58 +0900

    works on linux

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import flet as ft
+from flet.utils import is_windows
 
-from page.plot import Plot, PlotCpu, PlotMemory
+from page.plot import PlotCpu, PlotMemory
 
 async def main(page: ft.Page):
 
@@ -9,8 +10,9 @@ async def main(page: ft.Page):
     page.window_title_bar_hidden = True
     page.window_frameless = True
 
-    page.window_bgcolor = ft.colors.TRANSPARENT
-    page.bgcolor = ft.colors.TRANSPARENT
+    if is_windows():
+        page.window_bgcolor = ft.colors.TRANSPARENT
+        page.bgcolor = ft.colors.TRANSPARENT
 
     page.window_always_on_top = True
 
@@ -30,8 +32,6 @@ async def main(page: ft.Page):
     page.window_width = 48 + 40 + 96 + padding * controls_count * 4 + spacing * controls_count
     page.window_height = 64 * 2 + padding * controls_count * 2 + spacing * (controls_count - 1)
 
-    page.window_resizable = False
-
     page.window_visible = True
 
 
@@ -40,12 +40,13 @@ async def main(page: ft.Page):
             controls,
             spacing=4,
         ),
-        width=page.width,
-        height=page.height,
+        # width=page.width,
+        # height=page.height,
+        maximizable=False
     )
 
     await page.add_async(area)
 
 
 if __name__ == "__main__":
-    ft.app(target=main, view=ft.FLET_APP_HIDDEN)
+    ft.app(target=main, view=ft.FLET_APP)
