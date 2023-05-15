1963bf3b939655ca37d62f256f15dc167959dfb3
fmsyt <fms20yt@gmail.com>
2023-05-16 00:32:21 +0900

    set defaults

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import json
 
-
+from flet.utils import is_windows
 
 class Config:
     _instance = None
@@ -10,6 +10,26 @@ class Config:
             cls._instance = super().__new__(cls, *args, **kwargs)
         return cls._instance
 
+    def __init__(self) -> None:
+        self.initialize()
 
+    def initialize(self):
+        defaults = self.get_defaults()
+
+        self.application = defaults.application
+        self.plot = defaults.plot
+
+    def get_defaults(self):
+        return {
+            "application": {
+                "transparent": is_windows(),
+                "always_on_top": True,
+            },
+            "plot": {
+                "spacing": 4,
+            }
+        }
+
+    @NotImplemented
     def save(self):
         pass
