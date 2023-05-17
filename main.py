df285b9c602fdf94500659d118f188562d4f2ad2
fmsyt <fms20yt@gmail.com>
2023-05-17 17:25:55 +0900

    padding on linux

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from page.index import Index
 from page.settings import Settings
 
 from config import Config
-from flet.utils import is_windows
+from flet.utils import is_windows, is_linux
 
 def page_initialize(page: ft.Page):
     config = Config()
@@ -14,6 +14,9 @@ def page_initialize(page: ft.Page):
     controls_count = 2
 
     windows_title_bar_height = 10
+    linux_title_bar_height = 10
+
+    page.padding = 0
 
     page.window_width=48 + 40 + 96 + padding * controls_count * 4 + spacing * controls_count
     page.window_height=(64 + padding + spacing) * controls_count
@@ -23,6 +26,9 @@ def page_initialize(page: ft.Page):
 
     if is_windows() and config.application.hide_toolbar:
         page.window_height += windows_title_bar_height
+
+    if is_linux() and config.application.hide_toolbar:
+        page.window_height -= linux_title_bar_height
 
 async def main(page: ft.Page):
 
